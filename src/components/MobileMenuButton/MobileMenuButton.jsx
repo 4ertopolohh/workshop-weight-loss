0f22f89eb2e048795d226024ac2265ea33098548
4ertopolohh <isokolow503@gmail.com>
2025-10-09 18:38:17 +0300

    Обработка нажатия кнопки мобильного меню

--- src/components/MobileMenuButton/MobileMenuButton.jsx
+++ src/components/MobileMenuButton/MobileMenuButton.jsx
@@ -1,18 +1,20 @@
 import '../MobileMenuButton/MobileMenuButton.scss'
 import { Link, useLocation } from 'react-router-dom';
 
-import mobileMenuButtonBackground from '../../assets/images/pictures/mobileMenuButtonBackground.png'
-import mobileMenuButtonBackgroundActive from '../../assets/images/pictures/mobileMenuButtonBackgroundActive.png'
-import mobileMenuButtonBackgroundIIN from '../../assets/images/icons/mobileMenuIIN.svg'
-import mobileMenuButtonBackgroundIINActive from '../../assets/images/icons/mobileMenuIINActive.svg'
-import mobileMenuButtonHomePage from '../../assets/images/icons/mobileMenuHomePage.svg'
-
 const MobileMenuButton = ({ imgSrc, link, className = "default" }) => {
     const location = useLocation();
-    const isActive = location.pathname === link;
+    
+    const isActive = () => {
+        const currentPath = location.pathname || '/';
+        const itemPath = link.replace('/#', '');
+        return currentPath === itemPath;
+    };
     
     return(
-        <Link to={link} className={`mobileMenuButton ${className} ${isActive ? 'active' : ''}`}>
+        <Link 
+            to={link.replace('/#', '')} 
+            className={`mobileMenuButton ${className} ${isActive() ? 'active' : ''}`}
+        >
             <img src={imgSrc} alt="" loading='lazy'/>
         </Link>
     )
